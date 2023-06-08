b0497fe509ba32a5809bd435f68a20a7ad60241f
Odarell35 <leratomawash0@gmail.com>
2023-06-08 12:26:12 -0700

    removed spaces

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ import sys
 def main(*argv):
     len_av = len(sys.argv) - 1
     if len_av == 1:
-        print(" {:d} argument:".format(len_av))
+        print("{:d} argument:".format(len_av))
     elif len_av == 0:
-        print(" {:d} arguments.".format(len_av))
+        print("{:d} arguments.".format(len_av))
     else:
-        print(" {:d} arguments:".format(len_av))
+        print("{:d} arguments:".format(len_av))
     for len_av in range(len_av):
         print("{}: {}".format(len_av + 1, sys.argv[len_av + 1]))
 
