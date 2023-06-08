7af1595bb7a7a1bfd3563bd8e0fae23b709d097b
Odarell35 <leratomawash0@gmail.com>
2023-06-08 12:07:26 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 def main(*argv):
-    ac = 0
+    
     len_av = len(sys.argv) - 1
     if len_av == 1:
         print(" {:d} argument:".format(len_av))
@@ -9,11 +9,9 @@ def main(*argv):
         print(" {:d} arguments.".format(len_av))
     else:
         print(" {:d} arguments:".format(len_av))
-    for x in sys.argv:
-        if (ac != 0):
-            print("{}: {}".format(ac, x))
-            ac = ac + 1
-
+    for len_av in range(len_av):
+          print("{}: {}".format(len_av + 1, sys.argv[len_av + 1]))
+                
 if __name__ == "__main__":
     main()
 
