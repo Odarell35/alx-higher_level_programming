9d0900166f1835ff12547d4702b7b95146bbceca
Odarell35 <leratomawash0@gmail.com>
2023-06-09 04:49:24 -0700

    retrieve element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    length = len(my_list)
+    for i in my_list:
+        if idx < 0 :
+            return None
+        elif idx >= length:
+            return None
+        else:
+            return my_list.pop(idx)
+            
+
+
+if __name__ == "__main__":
+    element_at(my_list, idx)
