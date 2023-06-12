4be522d084988d777de01b25baee83150042091d
Odarell35 <leratomawash0@gmail.com>
2023-06-12 05:44:29 -0700

    returns tuple length

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) > 0 else 0:
+        a = tuple_a[0]
+    if len(tuple_a) > 1:
+        b = tuple_a[1]
+    if len(tuple_b) > 0:
+        c = tuple_b[0]
+    if len(tuple_b) > 1:
+        d = tuple_b[1]
+    else:
+        0
+
+
+    results = (a + c, b + d)
+
+    return results
+
