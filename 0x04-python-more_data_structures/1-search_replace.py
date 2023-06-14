8ef472241d89ff4405a479a315ad89bf352218c5
Odarell35 <leratomawash0@gmail.com>
2023-06-14 13:26:46 -0700

    pycodesylt

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for x in  my_list:
+    for x in my_list:
         if x == search:
             new_list.append(replace)
         else:
             new_list.append(x)
     return new_list
-
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def uniq_add(my_list = []):
+def uniq_add(my_list=[]):
     sorted_list = list(set(my_list))
     addition = 0
     for x in sorted_list:
