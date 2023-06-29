ad680cd302769ce50db45acca1290da7912f4960
Odarell35 <leratomawash0@gmail.com>
2023-06-29 00:15:17 -0700

    fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-  2 """creating module square"""
-  3 
-  4 
-  5 class Square:
-  6     """creating square classs
-  7     with conditional statements"""
-  8     def __init__(self, size = 0):
-  9     """initialization"""
- 10         if size is not isdigit():
- 11             raise TypeError("size must be an intger")
- 12         elif size < 0:
- 13             raise ValueError("size must be >=0")
- 14         else:
- 15             self.__size__ = size
+"""creating module square"""
+
+
+class Square:
+    """creating square classs
+        with conditional statements"""
+    def __init__(self, size = 0):
+        """initialization"""
+        if type(size) is not int:
+            raise TypeError("size must be an intger")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size__ = size
