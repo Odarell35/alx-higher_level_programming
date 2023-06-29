0666acde164db7096d119e3b01b858e21fc5bdd1
Odarell35 <leratomawash0@gmail.com>
2023-06-29 01:08:25 -0700

    pycodestyle

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,6 +8,7 @@ class Square:
         """
         initialize square"""
         self.__size = size
+
     @property
     def size(self):
         return self.__size
@@ -16,7 +17,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value  < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
