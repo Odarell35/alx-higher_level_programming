557c1dae872d6659dc457a161870d9fcd2612393
Odarell35 <leratomawash0@gmail.com>
2023-06-29 00:51:25 -0700

    setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,7 +8,13 @@ class Square:
         """
         initialize square"""
 
-        if type(size) is not int:
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
