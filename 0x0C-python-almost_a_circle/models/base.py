c064d6f74d0ef40b345ded20300ee3c23b07e4b0
Odarell35 <leratomawash0@gmail.com>
2023-07-16 11:03:59 -0700

    task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,8 +3,14 @@
 
 
 class Base:
-"""class base"""
+	"""class base"""
 	__nb_objects = 0
 
 	def __init__(self, id=None):
-	"""class co
+		"""class constructor"""
+		if id is not None:
+			self.id = id
+		else:
+			Base.__nb_objects +=1
+			self.id = Base.__nb_objects
+
