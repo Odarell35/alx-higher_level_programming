86759303b781f3b0c653b09da49933ee58cf4740
Odarell35 <leratomawash0@gmail.com>
2023-07-17 09:10:35 -0700

    JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module: Base"""
 
+import json
+
 
 class Base:
 	"""class base"""
@@ -14,3 +16,36 @@ class Base:
 			Base.__nb_objects +=1
 			self.id = Base.__nb_objects
 
+	@staticmethod
+	def to_json_string(list_dictionaries):
+		"""returns the JSON string representation 
+		of list_dictionaries"""
+		if not list_dictionaries:
+			return "[]"
+		return json.dumps(list_dictionaries)
+		
+	@classmethod
+	def save_to_file(cls, list_objs):
+		""" dictionary to json"""
+		empty_list = []
+		with open (cls.__name__ + ".json", mode= "w", encoding="utf-8") as myfile:
+			if list_objs:
+				for objs in list_objs:
+					empty_list.append(obj.to_dictionary())
+			myfile.write(cls.to_json_string(empty_list))
+	@staticmethod
+	def from_json_string(json_string):
+		"""returns the list of the JSON string representation"""
+		if not json_string:
+			return "[]"
+		return json.loads(json_string)
+	@classmethod
+	def create(cls, **dictionary):
+		"""returns an instance with all
+		attributes already set"""
+		if cls.__name__ == 'Rectangle':
+			a = cls(1, 1)
+		if cls.__name__ == 'Square':
+			a = cls(1)
+		a.update(**dictionary)
+		return a 
