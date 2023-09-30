326bcaf522c2354e2b30b7f01731235223c82a4d
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-09-30 17:39:28 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const a = process.argv[2];
-const b = process.argv[3];
-function add (a, b) {
-  const parseda = parseInt(a);
-  const parsedb = parseInt(b);
-  console.log(parseda + parsedb);
-}
-add(a, b);
