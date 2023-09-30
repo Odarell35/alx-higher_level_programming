6476e94a9b220972fa235510512923013c83aafb
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-09-30 17:39:16 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const list = require('./100-data').list;
-let j = 0;
-const mapped = list.map(x => x * j++);
-console.log(list);
-console.log(mapped);
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-let c = 0;
-
-exports.logMe = function (item) {
-  console.log(c + ': ' + item);
-  c++;
-};
