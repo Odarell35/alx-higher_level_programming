24ba149fed0ca58df8c6aba6e57739c5f7c16ac8
Odarell35 <leratomawash0@gmail.com>
2023-11-13 00:46:28 -0800

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const argument = process.argv
+const length = argument.length
+
+if (length === 2) {
+	console.log("No argument")
+}
+if (length === 3) {
+	console.log("Argument found")
+}
+if (length > 3 ) {
+	console.log("Arguments found")
+}
