141e6447274a9c90ce64d8de090035614a1a5d1d
Odarell35 <leratomawash0@gmail.com>
2023-11-13 10:32:56 -0800

    semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const argument = process.argv
-const length = argument.length
+const argument = process.argv;
+const length = argument.length;
 
 if (length === 2) {
-	console.log("No argument")
+  console.log('No argument');
 }
 if (length === 3) {
-	console.log("Argument found")
+  console.log('Argument found');
 }
-if (length > 3 ) {
-	console.log("Arguments found")
+if (length > 3) {
+  console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,9 +2,8 @@
 
 const first = process.argv[2];
 const parsedInt = parseInt(first, 10);
-if (!isNaN(parsedInt)){
-console.log(`My number: ${parsedInt}`);
-}
-else {
-console.log("Not a number");
+if (!isNaN(parsedInt)) {
+  console.log(`My number: ${parsedInt}`);
+} else {
+  console.log('Not a number');
 }
