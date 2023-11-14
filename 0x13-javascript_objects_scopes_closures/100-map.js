def051d28218fd268ba68b56532bb6d65a46ea25
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-11-14 17:38:48 +0200

    mode

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 let j = 0;
-const mapped = list.map(x => x * j++);
+const map_list = list.map(x => x * j++);
 console.log(list);
-console.log(mapped);
+console.log(map_list);
