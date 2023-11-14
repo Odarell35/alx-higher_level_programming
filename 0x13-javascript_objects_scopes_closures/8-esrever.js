323df2b46e2f212259b5c530cf9844a63f6ebeca
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-11-14 17:47:21 +0200

    semistandard

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const new_list = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-	  new_list.push(list[i]);
-  }
-  return new_list;
+	const newlist = [];
+	for (let i = list.length - 1; i >= 0; i--)
+	{
+		new_list.push(list[i]);
+	}
+	return newlist;
 };
