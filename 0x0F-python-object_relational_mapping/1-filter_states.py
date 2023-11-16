3e31029e585cee79c2b344a4e70e98ce3d38ad34
Odarell35 <leratomawash0@gmail.com>
2023-11-16 12:58:15 -0800

    filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a script that lists all states 
+    from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+def state_list():
+    """list states"""
+    database = MySQLdb.connect(host="localhost",user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3], port=3306)
+    c = database.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    c.execute(query)
+    row = c.fetchall()
+    for rr in row:
+        print(rr)
+    c.close()
+    database.close()
+
+
+if __name__ == "__main__":
+    state_list()
