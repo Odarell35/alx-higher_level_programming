c27c446efe47c7fe9d2c6bed57010e515cb3ae60
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-11-17 00:39:15 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""a script that lists all states 
+"""a script that lists all states
     from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
+
 def state_name():
     """list states"""
-    database = MySQLdb.connect(host="localhost",user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3], port=3306)
+    database = MySQLdb.connect(host="localhost",
+                               user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               db=sys.argv[3],
+                               port=3306)
     c = database.cursor()
     name_search = sys.argv[4]
     query = "SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC"
