e2942e62fbe5b4313580918fcf9eda1c69e12535
Odarell35 <leratomawash0@gmail.com>
2023-11-17 13:22:50 -0800

    update and delete

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""lists all State objects
+    from the database hbtn_0e_6_usa"""
+import sys
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+def all_state(State):
+    """lists all states"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database_name), pool_pre_ping=True)
+    s = Session(engine)
+    result =  s.query(State).filter_by(id=2).first()
+    result.name = "New Mexico"
+    
+    result = s.add(result)
+    s.commit()
+    s.close()
+
+
+if __name__ == "__main__":
+    all_state(State)
+
