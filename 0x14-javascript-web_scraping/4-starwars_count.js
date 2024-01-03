b17d8c4802bd90af1d63866ea702c35bc0514387
Odarell35 <leratomawasha0@gmail.com>
2024-01-03 07:12:34 -0800

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,16 @@ const apiUrl = process.argv[2];
 const characterId = '18'; // Wedge Antilles' character ID
 
 request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
+	if (error) {
+	console.error(error);
+	return;
+	}
 
-  const filmsData = JSON.parse(body).results;
-  const moviesWithWedgeAntilles = filmsData.filter(film =>
-    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-  );
+	const filmsData = JSON.parse(body).results;
+	console.log(filmsData.reduce(count, movie) => {
+		return movie.characters.find((character) => character.endsWith('/18/'))
+		? count + 1
+		: count;
+	}, 0);
 
-  console.log(`Number of movies where Wedge Antilles is present: ${moviesWithWedgeAntilles.length}`);
 });
