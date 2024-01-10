fa311824ffa4b5da9c08ae4b96972b15aa089513
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-01-10 19:16:04 +0200

    Update 0-script.js

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,3 @@
 // updates the text color of the <header> element to red (#FF0000)
-var elementt = document.querySelector('header');
+var element = document.querySelector('header');
 element.style.color = '#FF0000';
